a836c37e3a427470ce04358a61799a41f0053e94
Eran Rundstein <eran@rundste.in>
2022-03-09 00:30:16 -0800

    initial work on stubbing out SetMintConfigTx

--- peers/src/consensus_msg.rs
+++ peers/src/consensus_msg.rs
@@ -9,7 +9,7 @@ use mc_consensus_scp::Msg;
 use mc_crypto_digestible::{DigestTranscript, Digestible, MerlinTranscript};
 use mc_crypto_keys::{Ed25519Pair, Ed25519Signature, KeyError, Signer, Verifier};
 use mc_ledger_db::Ledger;
-use mc_transaction_core::{tx::TxHash, BlockID};
+use mc_transaction_core::{mint::SetMintConfigTx, tx::TxHash, BlockID};
 use serde::{Deserialize, Serialize};
 use std::{convert::TryFrom, hash::Hash, result::Result as StdResult};
 
@@ -19,6 +19,7 @@ use std::{convert::TryFrom, hash::Hash, result::Result as StdResult};
 )]
 pub enum ConsensusValue {
     TxHash(TxHash),
+    SetMintConfigTx(SetMintConfigTx),
 }
 
 impl From<TxHash> for ConsensusValue {
