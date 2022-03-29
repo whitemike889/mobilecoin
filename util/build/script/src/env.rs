006c1375f135f9ad8bd794f8ae351557e7eaaeb7
Nick Santana <nick@mobilecoin.com>
2022-03-29 22:36:31 +0000

    copy and paste is not a design pattern
    Struggling to get the sgx code to compile with a custom target.
    Had to use the fortanix vendor name because of libc looking for it *and*
    sgx.
    turned off the target-family check, not sure it's actually needed to be
    turned off but wanted to avoid using the unix file in libc

--- util/build/script/src/env.rs
+++ util/build/script/src/env.rs
@@ -178,7 +178,7 @@ pub struct Environment {
     target_arch: String,
     target_endian: Endianness,
     target_env: String,
-    target_family: TargetFamily,
+    // target_family: TargetFamily,
     target_has_atomic: HashSet<String>,
     target_has_atomic_load_store: HashSet<String>,
     target_os: String,
@@ -353,11 +353,11 @@ impl Environment {
             )?,
             target_env: var(ENV_CARGO_CFG_TARGET_ENV)
                 .map_err(|e| EnvironmentError::Var(ENV_CARGO_CFG_TARGET_ENV.to_owned(), e))?,
-            target_family: TargetFamily::try_from(
-                var(ENV_CARGO_CFG_TARGET_FAMILY)
-                    .map_err(|e| EnvironmentError::Var(ENV_CARGO_CFG_TARGET_FAMILY.to_owned(), e))?
-                    .as_ref(),
-            )?,
+            // target_family: TargetFamily::try_from(
+            //     var(ENV_CARGO_CFG_TARGET_FAMILY)
+            //         .map_err(|e| EnvironmentError::Var(ENV_CARGO_CFG_TARGET_FAMILY.to_owned(), e))?
+            //         .as_ref(),
+            // )?,
             target_features: var(ENV_CARGO_CFG_TARGET_FEATURE)
                 .map_err(|e| EnvironmentError::Var(ENV_CARGO_CFG_TARGET_FEATURE.to_owned(), e))?
                 .split(',')
@@ -549,9 +549,9 @@ impl Environment {
     }
 
     /// Get the target architecture family
-    pub fn target_family(&self) -> TargetFamily {
-        self.target_family
-    }
+    // pub fn target_family(&self) -> TargetFamily {
+    //     self.target_family
+    // }
 
     /// Get a reference to the target feature set
     pub fn target_features(&self) -> &HashSet<String> {
