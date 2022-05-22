9f9acb0df711a0af54ec69b9ce9a9c4f4670ce27
Jason Greathouse <jgreat@jgreat.me>
2022-05-22 11:09:40 -0500

    integrate latest candidate-1.2 - 37

--- .internal-ci/test/mobilecoind-grpc-drain-accounts.sh
+++ .internal-ci/test/mobilecoind-grpc-drain-accounts.sh
@@ -4,24 +4,66 @@
 # Wrapper around the mobilecoind drain-accounts.py to set up environment for testing.
 #
 
-strategies_dir=/tmp/drain-accounts/strategies
-keys_dir="${strategies_dir}/keys"
-fog_keys_dir="${strategies_dir}/fog_keys"
+set -e
 
-mkdir -p "${keys_dir}"
-mkdir -p "${fog_keys_dir}"
+usage()
+{
+    echo "Usage:"
+    echo "${0} --src <dir> --dst <dir>"
+    echo "    --src - source keys directory (keys to drain)"
+    echo "    --dst - destination keys directory (keys to fund)"
+    echo "    --token-id - token id to transfer"
+}
 
-echo "-- Copy account keys"
-echo ""
-for i in {0..6}
+is_set()
+{
+    var_name="${1}"
+    if [ -z "${!var_name}" ]
+    then
+        echo "${var_name} is not set."
+        usage
+        exit 1
+    fi
+}
+
+while true
 do
-    # shellcheck disable=SC2086 # yes we want globs
-    cp /tmp/sample_data/keys/*_${i}.* "${keys_dir}"
-    # shellcheck disable=SC2086 # yes we want globs
-    cp /tmp/sample_data/fog_keys*_${i}.* "${fog_keys_dir}"
+    case "${1}" in
+        --help | -h)
+            usage
+            exit 0
+            ;;
+        --src )
+            src="${2}"
+            shift 2
+            ;;
+        --dst )
+            dst="${2}"
+            shift 2
+            ;;
+        --token-id )
+            token_id="${2}"
+            shift 2
+            ;;
+        --fee )
+            fee="${2}"
+            shift 2
+            ;;
+        *)
+            break
+            ;;
+    esac
 done
 
+is_set src
+is_set dst
+is_set token_id
+is_set fee
+
+strategies_dir=/tmp/drain-accounts/strategies
+
 # This uses some of the same lib py files as mobilecoind tests.
+mkdir -p "${strategies_dir}"
 cp /test/mobilecoind/strategies/* "${strategies_dir}"
 
 pushd "${strategies_dir}" >/dev/null || exit 1
@@ -60,10 +102,11 @@ echo ""
 echo "-- Run integration_test.py"
 echo ""
 python3 drain-accounts.py \
-    --key-dir "${keys_dir}" \
-    --dest-keys-dir "${fog_keys_dir}" \
+    --key-dir "${src}" \
+    --dest-key-dir "${dst}" \
     --mobilecoind-host "mobilecoind" \
     --mobilecoind-port 3229 \
-    --token-id 1
+    --token-id "${token_id}" \
+    --fee "${fee}"
 
 popd >/dev/null || exit 1
